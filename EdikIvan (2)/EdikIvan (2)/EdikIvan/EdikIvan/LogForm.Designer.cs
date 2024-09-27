1c3b61ddb3b94e56b30f188e827d74e2c48d2d2b
proh368 <ivan.chistyakov.13@gmail.com>
2024-09-27 23:08:37 +0400

    Version 2
    Added more new functions

---
+++ EdikIvan (2)/EdikIvan (2)/EdikIvan/EdikIvan/LogForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace EdikIvan
+{
+    partial class LogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            logLab = new Label();
+            passLab = new Label();
+            logtext = new TextBox();
+            passtext = new TextBox();
+            logBtn = new Button();
+            regBtn = new Button();
+            loadingProgressBar = new ProgressBar();
+            AnimationTimer = new System.Windows.Forms.Timer(components);
+            showPasswordBtn = new Button();
+            confirmPasstext = new TextBox();
+            confirmPassLab = new Label();
+            confirmShowPasswordBtn = new Button();
+            newPasstext = new TextBox();
+            confirmNewPasstext = new TextBox();
+            currentPasstext = new TextBox();
+            changePasswordBtn = new Button();
+            currentPassLab = new Label();
+            newPassLab = new Label();
+            confirmNewPassLab = new Label();
+            SuspendLayout();
+            // 
+            // logLab
+            // 
+            logLab.AutoSize = true;
+            logLab.Location = new Point(52, 224);
+            logLab.Name = "logLab";
+            logLab.Size = new Size(40, 15);
+            logLab.TabIndex = 0;
+            logLab.Text = "логин";
+            // 
+            // passLab
+            // 
+            passLab.AutoSize = true;
+            passLab.Location = new Point(52, 296);
+            passLab.Name = "passLab";
+            passLab.Size = new Size(47, 15);
+            passLab.TabIndex = 1;
+            passLab.Text = "пароль";
+            // 
+            // logtext
+            // 
+            logtext.Location = new Point(52, 259);
+            logtext.Name = "logtext";
+            logtext.Size = new Size(209, 23);
+            logtext.TabIndex = 2;
+            // 
+            // passtext
+            // 
+            passtext.Location = new Point(52, 329);
+            passtext.Name = "passtext";
+            passtext.Size = new Size(209, 23);
+            passtext.TabIndex = 3;
+            // 
+            // logBtn
+            // 
+            logBtn.AutoSize = true;
+            logBtn.Location = new Point(346, 460);
+            logBtn.Name = "logBtn";
+            logBtn.Size = new Size(86, 25);
+            logBtn.TabIndex = 4;
+            logBtn.Text = "вход";
+            logBtn.UseVisualStyleBackColor = true;
+            logBtn.Click += logBtn_Click;
+            // 
+            // regBtn
+            // 
+            regBtn.AutoSize = true;
+            regBtn.Location = new Point(82, 460);
+            regBtn.Name = "regBtn";
+            regBtn.Size = new Size(86, 25);
+            regBtn.TabIndex = 5;
+            regBtn.Text = "регистрация";
+            regBtn.UseVisualStyleBackColor = true;
+            regBtn.Click += regBtn_Click;
+            // 
+            // loadingProgressBar
+            // 
+            loadingProgressBar.Location = new Point(24, 431);
+            loadingProgressBar.Name = "loadingProgressBar";
+            loadingProgressBar.Size = new Size(737, 23);
+            loadingProgressBar.Style = ProgressBarStyle.Marquee;
+            loadingProgressBar.TabIndex = 7;
+            loadingProgressBar.Visible = false;
+            // 
+            // showPasswordBtn
+            // 
+            showPasswordBtn.AutoSize = true;
+            showPasswordBtn.Location = new Point(284, 329);
+            showPasswordBtn.Name = "showPasswordBtn";
+            showPasswordBtn.Size = new Size(58, 25);
+            showPasswordBtn.TabIndex = 8;
+            showPasswordBtn.Text = "Скрыть";
+            showPasswordBtn.UseVisualStyleBackColor = true;
+            // 
+            // confirmPasstext
+            // 
+            confirmPasstext.Location = new Point(52, 380);
+            confirmPasstext.Name = "confirmPasstext";
+            confirmPasstext.Size = new Size(209, 23);
+            confirmPasstext.TabIndex = 9;
+            // 
+            // confirmPassLab
+            // 
+            confirmPassLab.AutoSize = true;
+            confirmPassLab.Location = new Point(52, 362);
+            confirmPassLab.Name = "confirmPassLab";
+            confirmPassLab.Size = new Size(148, 15);
+            confirmPassLab.TabIndex = 10;
+            confirmPassLab.Text = "Введите пароль павторно";
+            // 
+            // confirmShowPasswordBtn
+            // 
+            confirmShowPasswordBtn.AutoSize = true;
+            confirmShowPasswordBtn.Location = new Point(284, 378);
+            confirmShowPasswordBtn.Name = "confirmShowPasswordBtn";
+            confirmShowPasswordBtn.Size = new Size(58, 25);
+            confirmShowPasswordBtn.TabIndex = 11;
+            confirmShowPasswordBtn.Text = "Скрыть";
+            confirmShowPasswordBtn.UseVisualStyleBackColor = true;
+            // 
+            // newPasstext
+            // 
+            newPasstext.Location = new Point(480, 331);
+            newPasstext.Name = "newPasstext";
+            newPasstext.Size = new Size(209, 23);
+            newPasstext.TabIndex = 12;
+            // 
+            // confirmNewPasstext
+            // 
+            confirmNewPasstext.Location = new Point(480, 380);
+            confirmNewPasstext.Name = "confirmNewPasstext";
+            confirmNewPasstext.Size = new Size(209, 23);
+            confirmNewPasstext.TabIndex = 13;
+            // 
+            // currentPasstext
+            // 
+            currentPasstext.Location = new Point(480, 259);
+            currentPasstext.Name = "currentPasstext";
+            currentPasstext.Size = new Size(209, 23);
+            currentPasstext.TabIndex = 14;
+            // 
+            // changePasswordBtn
+            // 
+            changePasswordBtn.AutoSize = true;
+            changePasswordBtn.Location = new Point(543, 460);
+            changePasswordBtn.Name = "changePasswordBtn";
+            changePasswordBtn.Size = new Size(112, 25);
+            changePasswordBtn.TabIndex = 15;
+            changePasswordBtn.Text = "изменить пароль";
+            changePasswordBtn.UseVisualStyleBackColor = true;
+            // 
+            // currentPassLab
+            // 
+            currentPassLab.AutoSize = true;
+            currentPassLab.Location = new Point(480, 224);
+            currentPassLab.Name = "currentPassLab";
+            currentPassLab.Size = new Size(92, 15);
+            currentPassLab.TabIndex = 16;
+            currentPassLab.Text = "Старый пароль";
+            // 
+            // newPassLab
+            // 
+            newPassLab.AutoSize = true;
+            newPassLab.Location = new Point(480, 296);
+            newPassLab.Name = "newPassLab";
+            newPassLab.Size = new Size(88, 15);
+            newPassLab.TabIndex = 17;
+            newPassLab.Text = "Новый пароль";
+            // 
+            // confirmNewPassLab
+            // 
+            confirmNewPassLab.AutoSize = true;
+            confirmNewPassLab.Location = new Point(480, 362);
+            confirmNewPassLab.Name = "confirmNewPassLab";
+            confirmNewPassLab.Size = new Size(149, 15);
+            confirmNewPassLab.TabIndex = 18;
+            confirmNewPassLab.Text = "Введите пароль повторно";
+            // 
+            // LogForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Chocolate;
+            ClientSize = new Size(784, 561);
+            Controls.Add(confirmNewPassLab);
+            Controls.Add(newPassLab);
+            Controls.Add(currentPassLab);
+            Controls.Add(changePasswordBtn);
+            Controls.Add(currentPasstext);
+            Controls.Add(confirmNewPasstext);
+            Controls.Add(newPasstext);
+            Controls.Add(confirmShowPasswordBtn);
+            Controls.Add(confirmPassLab);
+            Controls.Add(confirmPasstext);
+            Controls.Add(showPasswordBtn);
+            Controls.Add(loadingProgressBar);
+            Controls.Add(regBtn);
+            Controls.Add(logBtn);
+            Controls.Add(passtext);
+            Controls.Add(logtext);
+            Controls.Add(passLab);
+            Controls.Add(logLab);
+            Name = "LogForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LogForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label logLab;
+        private Label passLab;
+        private TextBox logtext;
+        private TextBox passtext;
+        private Button logBtn;
+        private Button regBtn;
+        private ProgressBar loadingProgressBar;
+        private System.Windows.Forms.Timer AnimationTimer;
+        private Button showPasswordBtn;
+        private TextBox confirmPasstext;
+        private Label confirmPassLab;
+        private Button confirmShowPasswordBtn;
+        private TextBox newPasstext;
+        private TextBox confirmNewPasstext;
+        private TextBox currentPasstext;
+        private Button changePasswordBtn;
+        private Label currentPassLab;
+        private Label newPassLab;
+        private Label confirmNewPassLab;
+    }
+}
---
+++ EdikIvan (2)/EdikIvan (2)/EdikIvan/EdikIvan/MainForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace EdikIvan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            RandomBtn = new Button();
+            ChooseBtn = new Button();
+            PicRand = new PictureBox();
+            ExitBtn = new Button();
+            ClearBtn = new Button();
+            loglab = new Label();
+            ColorBtn = new Button();
+            trackBar1 = new TrackBar();
+            WidthLbl = new Label();
+            SaveBtn = new Button();
+            statusStrip1 = new StatusStrip();
+            UndoBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)PicRand).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // RandomBtn
+            // 
+            RandomBtn.AutoSize = true;
+            RandomBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            RandomBtn.Location = new Point(492, 436);
+            RandomBtn.Name = "RandomBtn";
+            RandomBtn.Size = new Size(280, 47);
+            RandomBtn.TabIndex = 0;
+            RandomBtn.Text = "Случайная картинка";
+            RandomBtn.UseVisualStyleBackColor = true;
+            RandomBtn.Click += RandomBtn_Click;
+            // 
+            // ChooseBtn
+            // 
+            ChooseBtn.AutoSize = true;
+            ChooseBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ChooseBtn.Location = new Point(492, 489);
+            ChooseBtn.Name = "ChooseBtn";
+            ChooseBtn.Size = new Size(280, 47);
+            ChooseBtn.TabIndex = 1;
+            ChooseBtn.Text = "Выбор картинки-->";
+            ChooseBtn.UseVisualStyleBackColor = true;
+            ChooseBtn.Click += ChooseBtn_Click;
+            // 
+            // PicRand
+            // 
+            PicRand.Location = new Point(172, 9);
+            PicRand.Name = "PicRand";
+            PicRand.Size = new Size(600, 400);
+            PicRand.TabIndex = 2;
+            PicRand.TabStop = false;
+            PicRand.Paint += PicRand_Paint;
+            PicRand.MouseDown += PicRand_MouseDown;
+            PicRand.MouseMove += PicRand_MouseMove;
+            PicRand.MouseUp += PicRand_MouseUp;
+            // 
+            // ExitBtn
+            // 
+            ExitBtn.AutoSize = true;
+            ExitBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ExitBtn.Location = new Point(12, 489);
+            ExitBtn.Name = "ExitBtn";
+            ExitBtn.Size = new Size(104, 47);
+            ExitBtn.TabIndex = 3;
+            ExitBtn.Text = "Выход";
+            ExitBtn.UseVisualStyleBackColor = true;
+            ExitBtn.Click += ExitBtn_Click;
+            // 
+            // ClearBtn
+            // 
+            ClearBtn.AutoSize = true;
+            ClearBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ClearBtn.Location = new Point(362, 436);
+            ClearBtn.Name = "ClearBtn";
+            ClearBtn.Size = new Size(124, 47);
+            ClearBtn.TabIndex = 4;
+            ClearBtn.Text = "Стереть";
+            ClearBtn.UseVisualStyleBackColor = true;
+            ClearBtn.Click += ClearBtn_Click;
+            // 
+            // loglab
+            // 
+            loglab.AutoSize = true;
+            loglab.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            loglab.Location = new Point(302, 412);
+            loglab.Name = "loglab";
+            loglab.Size = new Size(184, 21);
+            loglab.TabIndex = 5;
+            loglab.Text = "Тут вы можете рисовать";
+            // 
+            // ColorBtn
+            // 
+            ColorBtn.AutoSize = true;
+            ColorBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ColorBtn.Location = new Point(274, 489);
+            ColorBtn.Name = "ColorBtn";
+            ColorBtn.Size = new Size(212, 47);
+            ColorBtn.TabIndex = 6;
+            ColorBtn.Text = "Поменять цвет";
+            ColorBtn.UseVisualStyleBackColor = true;
+            ColorBtn.Click += ColorBtn_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(12, 33);
+            trackBar1.Maximum = 20;
+            trackBar1.Minimum = 1;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(154, 45);
+            trackBar1.TabIndex = 5;
+            trackBar1.Value = 1;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // WidthLbl
+            // 
+            WidthLbl.AutoSize = true;
+            WidthLbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            WidthLbl.Location = new Point(31, 9);
+            WidthLbl.Name = "WidthLbl";
+            WidthLbl.Size = new Size(107, 21);
+            WidthLbl.TabIndex = 7;
+            WidthLbl.Text = "Ширина пера";
+            // 
+            // SaveBtn
+            // 
+            SaveBtn.AutoSize = true;
+            SaveBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            SaveBtn.Location = new Point(12, 383);
+            SaveBtn.Name = "SaveBtn";
+            SaveBtn.Size = new Size(125, 47);
+            SaveBtn.TabIndex = 8;
+            SaveBtn.Text = "Скачать";
+            SaveBtn.UseVisualStyleBackColor = true;
+            SaveBtn.Click += SaveBtn_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 539);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(784, 22);
+            statusStrip1.TabIndex = 9;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // UndoBtn
+            // 
+            UndoBtn.AutoSize = true;
+            UndoBtn.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            UndoBtn.Location = new Point(121, 436);
+            UndoBtn.Name = "UndoBtn";
+            UndoBtn.Size = new Size(235, 47);
+            UndoBtn.TabIndex = 10;
+            UndoBtn.Text = "Вернуть рисунок";
+            UndoBtn.UseVisualStyleBackColor = true;
+            UndoBtn.Click += UndoBtn_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Chocolate;
+            ClientSize = new Size(784, 561);
+            Controls.Add(UndoBtn);
+            Controls.Add(statusStrip1);
+            Controls.Add(SaveBtn);
+            Controls.Add(WidthLbl);
+            Controls.Add(trackBar1);
+            Controls.Add(ColorBtn);
+            Controls.Add(loglab);
+            Controls.Add(ClearBtn);
+            Controls.Add(ExitBtn);
+            Controls.Add(PicRand);
+            Controls.Add(ChooseBtn);
+            Controls.Add(RandomBtn);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)PicRand).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button RandomBtn;
+        private Button ChooseBtn;
+        private PictureBox PicRand;
+        private Button ExitBtn;
+        private Button ClearBtn;
+        private Label loglab;
+        private Button ColorBtn;
+        private TrackBar trackBar1;
+        private Label WidthLbl;
+        private Button SaveBtn;
+        private StatusStrip statusStrip1;
+        private Button UndoBtn;
+    }
+}
